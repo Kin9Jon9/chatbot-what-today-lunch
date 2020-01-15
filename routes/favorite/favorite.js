7cb2def81f90f607809a02bb8064f775f84d2016
kin9jon9 <cvj9883@naver.com>
2020-01-15 03:09:08 +0000

    make foodDb.js

--- routes/favorite/favorite.js
+++ routes/favorite/favorite.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var dbconfig   = require('../../config/database');
 var userCheck = require('../../modules/userCheck');
+var foodDb = require('../../modules/foodDb');
 var con = mysql.createConnection(dbconfig);
 var router = express.Router();
 
@@ -10,6 +11,8 @@ router.post('/set',async function (req,res){
 	
 	//유저 식별키를 받아옴
 	const user = req.body.userRequest.user.properties.plusfriendUserKey;
+	//유저 발화문을 받아옴
+	const PreferedFood = req.body.userRequest.utterance;
 	
 	//유저 존재 여부 확인
 	const type =! await userCheck.check(user);
@@ -18,6 +21,12 @@ router.post('/set',async function (req,res){
 	if(type) await userCheck.add(user);
 	else console.log('유저키가 있어서 추가하지 않았습니다.');
 	
+	
+	console.log('데이터 추가중 ...');
+	//유저가 입력한 음식을 DB에 저장
+	await foodDb.add(PreferedFood);
+		
+	console.log(PreferedFood)
 	
 	const result = {
 		version : '2.0',
@@ -29,8 +38,22 @@ router.post('/set',async function (req,res){
 });
 
 //delete favorite Keyword
-router.post('/del',(req, res)=>{
-	res.send('0');
+router.post('/del', async (req, res)=>{
+	
+	//유저 식별키를 받아옴
+	const user = req.body.userRequest.user.properties.plusfriendUserKey;
+	//유저 발화문을 받아옴
+	const PreferedFood = req.body.userRequest.utterance;
+	
+	console.log('데이터를 삭제합니다.');
+	await foodDb.delete(PreferedFood);
+	
+	const result = {
+		version : '2.0',
+		data : '데이터가 삭제 되었습니다!'
+	}
+	
+	res.status(200).send(result);
 });
 
 module.exports = router;
